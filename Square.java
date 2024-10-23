705fc2b27111896d3ac3c28c84c77e749aa8372b
Sameer1539 <118702965+Sameer1539@users.noreply.github.com>
2024-10-23 12:27:25 -0400

    feat: implement the Square class

---
+++ Square.java
@@ -0,0 +1,11 @@
+public class Square {
+	double sideLength;
+	
+	public Square(double sideLength) {
+		this.sideLength = sideLength;
+	}
+	
+	public double perimeter() {
+		return sidelength * 4;
+	}
+}
